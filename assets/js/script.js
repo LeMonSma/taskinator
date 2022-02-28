41e0a0a9609f8efa7624fde3dd6ccf6f5104b984
Lemon Sama <burrisc24@gmail.com>
2022-02-28 14:27:22 -0500

    task can now be logged and saved to the list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
-var buttonEl = document.querySelector("#save-task"); 
-var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+let formEl = document.querySelector("#task-form");
+let tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() { 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+let createTaskHandler = function(event) {
+    event.preventDefault();
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-buttonEl.addEventListener("click", createTaskHandler);
+    // create list item
+    let listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    let taskInfoEl = document.createElement("div");
+    // create div to hold task info and add to list item
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
